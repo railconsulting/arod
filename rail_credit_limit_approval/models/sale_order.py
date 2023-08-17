b2fc8e2f33966a5ac90be5a4045701bfd871a74a
KevinITGT <78069008+KevinITGT@users.noreply.github.com>
2023-08-16 19:48:40 -0600

    update

--- rail_credit_limit_approval/models/sale_order.py
+++ rail_credit_limit_approval/models/sale_order.py
@@ -19,4 +19,13 @@ class SaleOrder(models.Model):
         readonly=True, copy=False, index=True,
         tracking=3,
         default='draft')
+    
+    credit_limit_restriction = fields.Boolean(compute='_compute_credit_limit_restriction')
 
+    @api.depends('partner_credit_warning','partner_id','partner_id.use_partner_credit_limit')
+    def _compute_credit_limit_restriction(self):
+        for r in self:
+            restriction = False
+            if len(r.partner_credit_warning.replace(' ','')) > 0 and r.partner_id.use_partner_credit_limit:
+                restriction = True
+            r.credit_limit_restriction = restriction
