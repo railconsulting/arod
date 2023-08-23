353841d28246b96c1a3b7609569d477094044f19
KevinITGT <78069008+KevinITGT@users.noreply.github.com>
2023-08-23 17:05:04 -0600

    first commit xml import

---
+++ rail_import_xml_invoice/__manifest__.py
@@ -0,0 +1,30 @@
+{
+    'name': "Importar facturas de proveedor via XML",
+    'summary': """""",
+    'description': """
+    """,
+    'author': "Rail / Kevin Lopez",
+    'license': 'OPL-1',
+    'category': 'Location',
+    'version': '16.0.0.1',
+    'depends': [
+        'base',
+        'account',
+        'portal',
+        'purchase',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'security/security.xml',
+        'data/data_sequence.xml',
+        'views/account_move_views.xml',
+        'views/xml_import_invoice_views.xml',
+        #'views/portal_templates.xml',
+        'wizard/xml_import_wizard_views.xml',
+    ],
+    'external_dependencies': {
+        'python': [
+            'cfdiclient',
+        ],
+    },
+}
