cca3a06b61fe1eb13af53a60c1d11c7614275f06
KevinITGT <78069008+KevinITGT@users.noreply.github.com>
2023-08-25 10:01:02 -0600

    first commit data clear

--- rail_data_clear/__manifest__.py
+++ rail_data_clear/__manifest__.py
@@ -1,24 +1,13 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-# Copyright (C) 2018 Bonainfo <guoyihot@outlook.com>
-# All Rights Reserved
-#
-##############################################################################
+
 {
-    'author': 'Dmmsys 124358678@qq.com ',
-    'website': 'www.bonainfo.com,www.dmmsys.com',
+    'name': 'Data Clear Tools V16',
+    'summary': """A powerful testing tool.Easily clear any odoo object data what you want. """,
+    'author': 'Rail / Kevin Lopez ',
+    'website': 'https://www.rail.com.mx',
     'version': '2.0',
     'category': 'Extra Tools',
     'license': 'OPL-1',
-    'support': '124358678@qq.com, bower_guo@msn.com',
-    'price': 28.00,
-    'currency': 'EUR',
-    'images': ['static/description/main_banner.png'],
-
-    'name': 'Data Clear Tools V16',
-    'summary': """A powerful testing tool.Easily clear any odoo object data what you want. """,
-    'description': """Business Testing Data Clear. You can define default model group list by yourself to help your work. """,
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
@@ -29,8 +18,5 @@
         'security/ir.model.access.csv',
         'views/clear_data_views.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        #'demo/demo.xml',
-    ],
+
 }
