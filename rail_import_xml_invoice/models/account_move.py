829e6015f8129f2245d62c9d317871fbbfdfc524
KevinITGT <78069008+KevinITGT@users.noreply.github.com>
2023-11-29 12:07:11 -0600

    add validation on bill move posting

--- rail_import_xml_invoice/models/account_move.py
+++ rail_import_xml_invoice/models/account_move.py
@@ -4,6 +4,9 @@ import base64
 from lxml import objectify, etree
 from odoo.exceptions import UserError, ValidationError
 from odoo.tools import float_is_zero
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 class AccountTax(models.Model):
@@ -41,17 +44,17 @@ class AccountMove(models.Model):
             precision = self.currency_id.decimal_places
             result = self.xml_file
             #data = base64.decodestring(result)
-            data = base64.b64decode(result)
-            fobj = tempfile.NamedTemporaryFile(delete=False)            
-            namespaces = {'cfdi': 'http://www.sat.gob.mx/cfd/3'}
+            data = base64.b64decode(result)            
+            #namespaces = {'cfdi': 'http://www.sat.gob.mx/cfd/3'}
             try:
+                fobj = tempfile.NamedTemporaryFile(delete=False)
                 fname = fobj.name
                 fobj.write(data)
                 fobj.close()
                 file_xml = open(fname, "r")
                 tree = objectify.fromstring(file_xml.read().encode())
+                _logger.critical("try")
             except:
-
                 try:
                     recovering_parser = etree.XMLParser(recover=True)
                     tree = None
@@ -67,8 +70,16 @@ class AccountMove(models.Model):
             # 	raise UserError(
             # 		_('File %s is not xml type, please remove from list') % (
             # 			data_file.display_name))
-            if self._get_stamp_data(tree) is None:
-                if self.partner_id.vat != tree.Emisor.get('Rfc'):
+            if tree.findall('{http://www.sat.gob.mx/cfd/4}Complemento'):
+                namespaces = {'cfdi': 'http://www.sat.gob.mx/cfd/4'}
+            else:
+                namespaces = {'cfdi': 'http://www.sat.gob.mx/cfd/3'}
+            tfd = self._get_stamp_data(tree)
+            if tfd is None:
+                raise UserError(
+                    _("No se ha encontrado el UUID en el XML")
+                )
+                """ if self.partner_id.vat != tree.Emisor.get('Rfc'):
                     raise UserError(
                         _("The provider's RFC (%s) does not match the RFC (%s) of the "
                           "attached xml") % (self.partner_id.vat, tree.Emisor.get('Rfc'))
@@ -85,23 +96,19 @@ class AccountMove(models.Model):
 
                 if not float_is_zero(self.amount_untaxed - sub_total,
                                      precision_digits=precision):
-                    """
                     raise UserError(
                         _("The sub-total amount (%s) of the invoice does not match the "
                           "sub-total amount (%s) of the attached xml") %
                         (str(self.amount_untaxed), sub_total)
                     )
-                    """
 
                 if not float_is_zero(self.amount_total - float(tree.get('Total')),
                                      precision_digits=precision):
-                    """
                     raise UserError(
                         _("The total amount (%s) of the invoice does not match the total "
                           "amount (%s) of the attached xml") %
                         (str(self.amount_total), tree.get('Total'))
                     )
-                    """
 
                 if self.currency_id.name != tree.get('Moneda'):
                     raise UserError(
@@ -113,10 +120,8 @@ class AccountMove(models.Model):
                     raise UserError(
                         _("The invoice date (%s) does not match the date of the XML "
                           "attachment (%s)") % (str(self.invoice_date), date,)
-                    )
+                    ) """
             else:
-                tfd = self._get_stamp_data(tree)
-
                 if self.partner_id.vat != tree.xpath('cfdi:Emisor', namespaces=namespaces)[0].get('Rfc'):
                     raise UserError(
                         _("The provider's RFC (%s) does not match the RFC (%s) of the "
@@ -159,13 +164,38 @@ class AccountMove(models.Model):
                         _("The invoice date (%s) does not match the date of the XML "
                           "attachment (%s)") % (str(self.invoice_date), date,)
                     )
+                uuid = tfd.get('UUID')
+                invoice = self.env['account.move'].search([('ref', '=', uuid)], limit=1)
+                if invoice:
+                    raise UserError(
+                        _("El UUID del xml cargado ya se encuentra registrado. \n"
+                          + "Factura: (%s)"  % (invoice.name))
+                    )
+                else:
+                    self.ref = uuid
+
 
         return super(AccountMove, self).action_post()
 
-    @api.model
+    """ @api.model
     def _get_stamp_data(self, cfdi):
         self.ensure_one()
         complemento = cfdi.xpath("//cfdi:Complemento", namespaces={'cfdi': 'http://www.sat.gob.mx/cfd/3'})
+        if not complemento:#hasattr(cfdi, 'Complemento'):
+            return None
+        attribute = '//tfd:TimbreFiscalDigital'
+        namespace = {'tfd': 'http://www.sat.gob.mx/TimbreFiscalDigital'}
+        node = complemento[0].xpath(attribute, namespaces=namespace)
+        _logger.critical(str(node))
+        return node[0] if node else None """
+    
+    @api.model
+    def _get_stamp_data(self, cfdi):
+        self.ensure_one()
+        if cfdi.findall('{http://www.sat.gob.mx/cfd/4}Complemento'):
+            complemento = cfdi.xpath("//cfdi:Complemento", namespaces={'cfdi': 'http://www.sat.gob.mx/cfd/4'})
+        else:
+            complemento = cfdi.xpath("//cfdi:Complemento", namespaces={'cfdi': 'http://www.sat.gob.mx/cfd/3'})
         if not complemento:#hasattr(cfdi, 'Complemento'):
             return None
         attribute = '//tfd:TimbreFiscalDigital'
