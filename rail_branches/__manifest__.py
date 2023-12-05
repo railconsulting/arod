71028be545ae0d2d071085a5713955fc6ac8d014
KevinITGT <78069008+KevinITGT@users.noreply.github.com>
2023-12-04 19:14:23 -0600

    first commit

---
+++ rail_branches/__manifest__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Manejo de multiples unidades operativas',
+    'version': '16.0.0.1',
+    'category': 'Sales',
+    'summary': 'Manejos de sucursales / unidades operativas',
+    'author': 'Rail / Kevin Lopez',
+    'website': 'https://www.rail.com.mx',
+    'depends': ['base', 'sale_management', 'purchase', 'stock', 'account', 'purchase_stock','web'],
+    'uninstall_hook': '_uninstall_hook',
+    'data': [
+        'security/branch_security.xml',
+        'security/multi_branch.xml',
+        'security/ir.model.access.csv',
+        'views/res_branch_view.xml',
+        'views/res_users.xml',
+        'views/sale_order.xml',
+        'views/stock_picking.xml',
+        'views/stock_move.xml',
+        'views/account_invoice.xml',
+        'views/purchase_order.xml',
+        'views/stock_warehouse.xml',
+        'views/stock_location.xml',
+        'views/account_bank_statement.xml',
+        'wizard/account_payment.xml',
+        'views/product.xml',
+        'views/partner.xml',
+        'views/stock_quant_view.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'rail_branches/static/src/js/session.js',
+            'rail_branches/static/src/js/branch_service.js',
+            'rail_branches/static/src/xml/branch.xml'
+        ]
+    },
+    'license' : 'OPL-1',
+    'installable': True,
+    'auto_install': False,
+    'post_init_hook': 'post_init_hook',
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ rail_branches/models/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from . import branch
+from . import res_users
+from . import sale_order
+from . import stock_move
+from . import stock_picking
+from . import account_move
+from . import account_payment
+from . import purchase_order
+from . import stock_warehouse
+from . import stock_location
+from . import account_bank_statement
+from . import account_bank_statement_line
+from . import customer
+from . import product
+from . import ir_http
+from . import stock_quant
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
