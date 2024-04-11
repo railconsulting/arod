9824fb58c173b91b93c7f79aa1e2db7225300501
KevinITGT <78069008+KevinITGT@users.noreply.github.com>
2024-04-11 09:36:18 -0600

    merge from valoracion

--- rail_workcenter_account/models/employee.py
+++ rail_workcenter_account/models/employee.py
@@ -5,5 +5,5 @@ from odoo import models, fields
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
-    inventory_account_id = fields.Many2one('account.account', string="Cuenta produccion en proceso", required=True)
-    expense_account_id = fields.Many2one('account.account', string="Cuenta gasto", required=True)
+    inventory_account_id = fields.Many2one('account.account', string="Cuenta produccion en proceso")
+    expense_account_id = fields.Many2one('account.account', string="Cuenta gasto")
