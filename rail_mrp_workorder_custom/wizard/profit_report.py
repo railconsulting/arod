24a68cd48b3f59fd2df32134388fea17dc38afbf
KevinITGT <78069008+KevinITGT@users.noreply.github.com>
2024-05-31 14:02:55 -0600

    fix cell formats

--- rail_mrp_workorder_custom/wizard/profit_report.py
+++ rail_mrp_workorder_custom/wizard/profit_report.py
@@ -75,7 +75,7 @@ class SaleProfitReport(models.TransientModel):
             invoice_cost = sum(abs(p.value) for p in source_stocklayers)
             invoice_subtotal = sum(l.credit for l in inv_lines)
             invoice_tax = sum(l.price_total - l.price_subtotal for l in inv_lines)
-            invoice_perc = ((invoice_subtotal - invoice_cost) / invoice_subtotal) * 100
+            invoice_perc = ((invoice_subtotal - invoice_cost) / invoice_subtotal)
 
             #devs
             cv = invoice_cost / sum(l.quantity for l in inv_lines)
@@ -85,14 +85,14 @@ class SaleProfitReport(models.TransientModel):
             nc_net_cost = invoice_cost - nc_cost
             nc_tax = sum(l.price_total - l.price_subtotal for l in nc_lines_dev)
             nc_net_subtotal = invoice_subtotal - nc_subtotal
-            nc_porc = ((nc_net_subtotal - nc_net_cost) / nc_net_subtotal) * 100
+            nc_porc = ((nc_net_subtotal - nc_net_cost) / nc_net_subtotal)
 
             #bonif
             bonus_subtotal =  sum(abs(l.debit) for l in nc_lines_bon)
             bonus_cost = nc_net_cost
             bonus_net_subtotal = nc_net_subtotal - bonus_subtotal
             bonus_tax = sum(l.price_total - l.price_subtotal for l in nc_lines_bon)
-            bonus_perc = ((bonus_net_subtotal - bonus_cost) / bonus_net_subtotal) * 100
+            bonus_perc = ((bonus_net_subtotal - bonus_cost) / bonus_net_subtotal)
 
             #total
             ctrl1 = sum(l.quantity for l in inv_lines)
@@ -102,13 +102,13 @@ class SaleProfitReport(models.TransientModel):
             net = subtotal + tax
             cost_subtotal = nc_net_cost
             profit = subtotal - cost_subtotal
-            profit_perc = ((subtotal - cost_subtotal) / subtotal) * 100
+            profit_perc = ((subtotal - cost_subtotal) / subtotal)
 
             #delivery
             free_delivery = sum(o.shipping_total for o in source_orders.filtered(lambda x: x.shipping_type == x.shipping_type == 'free'))
             customer_delivery = sum(o.shipping_total for o in source_orders.filtered(lambda x: x.shipping_type == x.shipping_type == 'customer'))
             if bonus_cost > 0 and free_delivery > 0 and customer_delivery > 0:
-                delivery_perc = ((bonus_cost / ((free_delivery + customer_delivery) - bonus_cost)) / bonus_cost) * 100
+                delivery_perc = ((bonus_cost / ((free_delivery + customer_delivery) - bonus_cost)) / bonus_cost)
             else:
                 delivery_perc = 0
 
@@ -190,6 +190,30 @@ class SaleProfitReport(models.TransientModel):
             'text_wrap': True,
             'num_format': '[$'+company.currency_id.symbol+']#,##0.00',
         })
+        table_footer_int = book.add_format({
+            'border': 1,
+            'bold':True,
+            'bg_color': '#CC9D2E',
+            'font_color': 'black',
+            'align': 'right',
+            'valign': 'vcenter',
+            'font_name': 'Calibri',
+            'font_size': 12,
+            'text_wrap': True,
+            'num_format': 1,
+        })
+        table_footer_per = book.add_format({
+            'border': 1,
+            'bold':True,
+            'bg_color': '#CC9D2E',
+            'font_color': 'black',
+            'align': 'right',
+            'valign': 'vcenter',
+            'font_name': 'Calibri',
+            'font_size': 12,
+            'text_wrap': True,
+            'num_format': 10,
+        })
         calibri_12 = book.add_format({
             'font_name': 'Calibri',
             'font_size': 11,
@@ -210,6 +234,18 @@ class SaleProfitReport(models.TransientModel):
             'font_size': 10,
             'align': 'right',
             'num_format': '[$'+company.currency_id.symbol+']#,##0.00',
+        })
+        calibri_10_int = book.add_format({
+            'font_name': 'Calibri',
+            'font_size': 10,
+            'align': 'right',
+            'num_format': 1,
+        })
+        calibri_10_per = book.add_format({
+            'font_name': 'Calibri',
+            'font_size': 10,
+            'align': 'right',
+            'num_format': 10,
         })
         calibri_9 = book.add_format({
             'font_name': 'Calibri',
@@ -274,28 +310,28 @@ class SaleProfitReport(models.TransientModel):
             sheet.write(row, 4, d['4'], calibri_10)
             sheet.write(row, 5, d['5'], calibri_10)
             sheet.write(row, 6, d['6'], calibri_10)
-            sheet.write(row, 7, d['7'], calibri_10)
+            sheet.write(row, 7, d['7'], calibri_10_per)
             sheet.write(row, 8, d['8'], calibri_10)
             sheet.write(row, 9, d['9'], calibri_10)
             sheet.write(row, 10, d['10'], calibri_10)
             sheet.write(row, 11, d['11'], calibri_10)
-            sheet.write(row, 12, d['12'], calibri_10)
+            sheet.write(row, 12, d['12'], calibri_10_per)
             sheet.write(row, 13, d['13'], calibri_10)
             sheet.write(row, 14, d['14'], calibri_10)
             sheet.write(row, 15, d['15'], calibri_10)
-            sheet.write(row, 16, d['16'], calibri_10)
-            sheet.write(row, 17, d['17'], calibri_10)
-            sheet.write(row, 18, d['18'], calibri_10)
+            sheet.write(row, 16, d['16'], calibri_10_per)
+            sheet.write(row, 17, d['17'], calibri_10_int)
+            sheet.write(row, 18, d['18'], calibri_10_int)
             sheet.write(row, 19, d['19'], calibri_10)
             sheet.write(row, 20, d['20'], calibri_10)
             sheet.write(row, 21, d['21'], calibri_10)
             sheet.write(row, 22, d['22'] , calibri_10)
             sheet.write(row, 23, d['23'], calibri_10)
-            sheet.write(row, 24, d['24'], calibri_10)
+            sheet.write(row, 24, d['24'], calibri_10_per)
             sheet.write(row, 25, d['25'], calibri_10)
             sheet.write(row, 26, d['26'], calibri_10)
             sheet.write(row, 27, d['27'], calibri_10)
-            sheet.write(row, 28, d['28'], calibri_10)
+            sheet.write(row, 28, d['28'], calibri_10_per)
             tot5 += d['5']
             tot6 += d['6']
             tot7 += d['7']
@@ -322,23 +358,24 @@ class SaleProfitReport(models.TransientModel):
         sheet.merge_range(row, 0, row, 4, "TOTAL REGISTROS: " + str(len(data)), table_footer)
         sheet.write(row, 5, tot5, table_footer)
         sheet.write(row, 6, tot6, table_footer)
-        sheet.write(row, 7, tot7, table_footer)
+        sheet.write(row, 7, tot7, table_footer_per)
         sheet.write(row, 8, tot8, table_footer)
         sheet.write(row, 9, tot9, table_footer)
         sheet.write(row, 10, tot10, table_footer)
         sheet.write(row, 11, tot11, table_footer)
-        sheet.write(row, 12, tot12, table_footer)
+        sheet.write(row, 12, tot12, table_footer_per)
         sheet.write(row, 13, tot13, table_footer)
         sheet.write(row, 14, tot14, table_footer)
         sheet.write(row, 15, tot15, table_footer)
-        sheet.write(row, 16, tot16, table_footer)
-        sheet.write(row, 17, tot16, table_footer)
-        sheet.write(row, 18, tot18, table_footer)
+        sheet.write(row, 16, tot16, table_footer_per)
+        sheet.write(row, 17, tot17, table_footer_int)
+        sheet.write(row, 18, tot18, table_footer_int)
         sheet.write(row, 19, tot19, table_footer)
         sheet.write(row, 20, tot20, table_footer)
         sheet.write(row, 21, tot21, table_footer)
         sheet.write(row, 22, tot22, table_footer)
         sheet.write(row, 23, tot23, table_footer)
+        sheet.write(row, 24, tot24, table_footer_per)
 
 
         sheet.set_column(0,0,20)
