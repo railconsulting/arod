77c38d8991f312885179600f8f984fa3c7c4b400
FranciscoRodriguezBotello <105890754+FranciscoRodriguezBotello@users.noreply.github.com>
2024-07-05 17:04:20 -0600

    Update tablas_nomina.py

--- nomina_cfdi/models/tablas_nomina.py
+++ nomina_cfdi/models/tablas_nomina.py
@@ -13,7 +13,6 @@ class TablasAntiguedadesLine(models.Model):
     vacaciones = fields.Float('Vacaciones/Días') 
     prima_vac = fields.Float('Prima vacacional (%)')
     aguinaldo = fields.Float('Aguinaldo/Días')
-   # factor_integracion = fields.Float('Factor integracion')
 
 class TablasGeneralLine(models.Model):
     _name = 'tablas.general.line'
